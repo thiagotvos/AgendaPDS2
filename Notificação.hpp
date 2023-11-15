19ad30502aea86e76912c99f3bd21f05cc35db51
Samuel Lima Horta <148572679+samucao1803@users.noreply.github.com>
2023-11-15 17:18:35 -0300

    Reestruturação Notificação.hpp

--- Notificação.hpp
+++ Notificação.hpp
@@ -1,32 +1,35 @@
 #pragma once
 #include <string>
-#include <list>
-   
+#include "Tarefa.hpp"
+#include "Lembrete.hpp"
+#include <ctime>
+#include <chrono>
+
 class Notificacao {
 public:
-    Notificacao(std::string& horario, std::string& mensagem, std::string& data );
-
-    std::string getdata() const;
-    std::string gethorario() const;
+    Notificacao(int AntecedenciaMinutos, int AntecedenciaHoras);
     
-    void adicionarMensagem (std::string texto);
-    //Adiciona uma mensagem especifica a notificação
+    int getAntecedenciaMinutos() const;
+    int getAntecedenciaHoras() const;
+
+    void setAntecedenciaMinutos(int NovaAntecedencia);
+    // redefine a antecedência desejada em minutos
+
+    void setAntecedenciaHoras(int NovaAntecedencia);
+    // redefine a antecedência desejada em horas
+
+    void notificarTarefa(Tarefa* tarefa);
+    // compara com o horário real e notifica tarefa (com certo tempo de antecedência ou no horário programado)
+
+    void notificarCompromisso(Compromisso* compromisso);
+    // compara com o horário real e notifica Compromisso (com certo tempo de antecedência ou no horário programado)
+
+    void notificarLembrete(Lembrete* lembrete);
+    // compara com o horário real e notifica Lembrete (com certo tempo de antecedência ou no horário programado)
 
 private:
-    std::string data;
-    std::string horario;
-    std::string mensagem;
-};
-
-class ListaNotificacao {
-public:
-    void adicionarNotificacao(Notificacao* notificacao);
-    //adiciona a notificação a lista de notificações
-
-    void removerNotificacao(Notificacao* notificacao);
-    //remove a notificação da lista de notificações
-
-private:
-    std::list<Notificacao> listadeNotificacao;
-
+    int AntecedenciaMinutos;
+    // de 0 a 59 min 
+    int AntecedenciaHoras;
+    // de 1 a 23 horas
 };
