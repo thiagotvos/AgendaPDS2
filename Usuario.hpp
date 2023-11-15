d9b0fb3495f040ced5e6c6778e85ebc7d8ed52cd
samucao1803 <samuel.horta18@gmail.com>
2023-11-14 21:18:38 -0300

    Adicionando classe usuario

---
+++ Usuario.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+   
+ class Usuario{
+  public:
+   Usuario(std::string& nome, std::string& sobrenome, std::string& idade,
+          std::string& email);
+    
+     std::string getnome() const;
+     std::string getemail() const; 
+
+     void mudaremail (std::string texto);
+   
+  private:
+     std::string nome;
+     std::string sobrenome;
+     std::string idade;
+     std::string email;
+
+};
