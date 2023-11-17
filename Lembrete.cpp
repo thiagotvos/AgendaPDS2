1f2e252832a5f95a9fb502ae60d6f3f6b18bde5d
felipeluccaTO <lipelucca100@gmail.com>
2023-11-17 09:28:14 -0300

    Implementaçoes dos metodos de Lembrete

---
+++ Lembrete.cpp
@@ -0,0 +1,34 @@
+#include "Lembrete.hpp"
+#include <string>
+
+std::string Lembrete::getdata() const {
+
+    // Retorna a data 
+    return this->data;
+}
+
+std::string Lembrete::gethorario() const {
+
+    // Retorna o horario
+    return this->horario;
+}
+
+void Lembrete::alterarMensagem(std::string nova_mensagem) {
+    
+    // Verifica se a nova mensagem esta vazia, ou se a nova mensagem eh igual a mensagem atual
+    if (!nova_mensagem.empty() || nova_mensagem != mensagem) {
+
+        // Atribui a nova mensagem
+        this->mensagem = nova_mensagem;
+    }
+}
+
+void ListaLembrete::adicionarLembrete(Lembrete* lembrete) {
+
+    // A fazer
+}
+
+void ListaLembrete::removerLembrete(Lembrete* lembrete) {
+
+    // A fazer
+}
