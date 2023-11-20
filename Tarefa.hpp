77d1ce1858c495c264b23c367728e375d1bfd3b2
Samuel Lima Horta <148572679+samucao1803@users.noreply.github.com>
2023-11-20 10:22:29 -0300

    Mudança no contrato de Tarefa.hpp

--- Tarefa.hpp
+++ Tarefa.hpp
@@ -9,11 +9,40 @@
      std::string getTitulo() const;
      unsigned getPrioridade() const;
      std::string getEstado() const; 
-
+   
+    /// @brief Muda o titulo de uma Tarefa/Compromisso
+    ///
+    /// @param NovoTitulo       O novo titulo a ser colocado
     void setTitulo(const std::string& novoTitulo);
+
+    /// @brief Muda a descrição de uma Tarefa/Compromisso
+    ///
+    /// @param NovaDescricao       A nova descrição a ser colocada
     void setDescricao(const std::string& novaDescricao);
+
+    /// @brief Muda a data de uma Tarefa/Compromisso
+    ///
+    /// @param NovaData       A nova data a ser colocada
+    ///
+    /// @attention A data informada deve estar no formato:
+    /// DD/MM/AAAA
+    ///
     void setData(const std::string& novaData);
+
+    /// @brief Muda a prioridade da Tarefa/Compromisso
+    ///
+    /// @param NovaPrioridade       A nova prioridade a ser colocada
+    ///
+    /// @attention A prioridade varia entre: 
+    /// 1 --> menos importante ; 2 --> importante; 3 --> muito importante.
     void setPrioridade(unsigned novaPrioridade);
+
+    /// @brief Muda o estado de uma Tarefa/Compromisso
+    ///
+    /// @param NovoEstado       O novo estado a ser colocado
+    ///
+    /// @attention O Estado varia entre:
+    /// feito ; em progresso; não feito.
     void setEstado(const std::string& novoEstado);
    
   private:
@@ -35,7 +64,18 @@ class Compromisso : public Tarefa {
     std::string getLocal() const;
     std::string getHorario() const;
 
+    /// @brief Muda o local de um Compromisso
+    ///
+    /// @param NovoLocal       O novo local a ser colocado 
     void setLocal(const std::string& novoLocal);
+
+    /// @brief Muda o horário de um Compromisso
+    ///
+    /// @param NovoHorario       O novo horário a ser colocado
+    ///
+    /// @attention O Horario informado deve estar no formato:
+    /// HH:MM:SS
+    ///
     void setHorario(const std::string& novoHorario);
 
   private:
