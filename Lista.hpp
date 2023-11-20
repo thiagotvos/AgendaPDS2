5089cfba7c7a499457a9a82eb8ececc5c3ccd895
Samuel Lima Horta <148572679+samucao1803@users.noreply.github.com>
2023-11-20 10:42:36 -0300

    Mudanças no contrato da Lista.hpp

--- Lista.hpp
+++ Lista.hpp
@@ -8,8 +8,18 @@ public:
     ListaTarefa();
     ~ListaTarefa();
 
+    /// @brief Adiciona uma Tarefa a Lista de Tarefas
+    ///
+    /// @param tarefa       a Tarefa a ser adicionada
     void adicionarTarefa(Tarefa* tarefa);
+
+    /// @brief Remove uma Tarefa da Lista de Tarefas
+    ///
+    /// @param tarefa       a Tarefa a ser removida
     void removerTarefa(Tarefa* tarefa);
+
+    /// @brief Mostra a Lista de Tarefas
+    ///
     void verTarefas();
 
 private:
@@ -21,8 +31,18 @@ public:
     ListaCompromisso();
     ~ListaCompromisso();
 
+    /// @brief Adiciona um Compromisso a Lista de Compromissos
+    ///
+    /// @param compromisso       O compromisso a ser adicionado.
     void adicionarCompromisso(Compromisso* compromisso);
+
+    /// @brief Remove um Compromisso da Lista de Compromissos
+    ///
+    /// @param compromisso       O compromisso a ser removido.
     void removerCompromisso(Compromisso* compromisso);
+
+    /// @brief Mostra os Compromisso da Lista de Compromissos
+    ///
     void verCompromissos();
 
 private:
