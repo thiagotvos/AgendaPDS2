0388f88141eb3c1b86f0d19dae36fcfbac0aa807
felipeluccaTO <lipelucca100@gmail.com>
2023-11-20 15:45:39 -0300

    Organizaçao de codigo

--- Tarefa.hpp
+++ Tarefa.hpp
@@ -1,85 +1,93 @@
 #pragma once
 #include <string>
+#include <list>
    
- class Tarefa {
-  public:
-   Tarefa(std::string& titulo, std::string& descricao, std::string& data,
-          unsigned prioridade, std::string& estado);
+class Tarefa {
+public:
+  Tarefa(std::string& titulo, std::string& descricao, std::string& data, unsigned prioridade, std::string& estado);
     
-     std::string getTitulo() const;
-     unsigned getPrioridade() const;
-     std::string getEstado() const; 
-   
-    /// @brief Muda o titulo de uma Tarefa/Compromisso
-    ///
-    /// @param NovoTitulo       O novo titulo a ser colocado
-    void setTitulo(const std::string& novoTitulo);
-
-    /// @brief Muda a descrição de uma Tarefa/Compromisso
-    ///
-    /// @param NovaDescricao       A nova descrição a ser colocada
-    void setDescricao(const std::string& novaDescricao);
-
-    /// @brief Muda a data de uma Tarefa/Compromisso
-    ///
-    /// @param NovaData       A nova data a ser colocada
-    ///
-    /// @attention A data informada deve estar no formato:
-    /// DD/MM/AAAA
-    ///
-    void setData(const std::string& novaData);
-
-    /// @brief Muda a prioridade da Tarefa/Compromisso
-    ///
-    /// @param NovaPrioridade       A nova prioridade a ser colocada
-    ///
-    /// @attention A prioridade varia entre: 
-    /// 1 --> menos importante ; 2 --> importante; 3 --> muito importante.
-    void setPrioridade(unsigned novaPrioridade);
-
-    /// @brief Muda o estado de uma Tarefa/Compromisso
-    ///
-    /// @param NovoEstado       O novo estado a ser colocado
-    ///
-    /// @attention O Estado varia entre:
-    /// feito ; em progresso; não feito.
-    void setEstado(const std::string& novoEstado);
-   
-  private:
-     std::string titulo;
-     std::string descricao;
-     std::string data;
-     unsigned prioridade;
-     std::string estado;
-      //3 tipos de estado: feito (deletar em seguida); em progresso; não feito (alertar)
-
+  /// @brief Busca pelo titulo da tarefa 
+  ///
+  /// @return String do titulo
+  std::string getTitulo() const;
+    
+  /// @brief Busca pela prioridade da tarefa
+  ///
+  /// @return Unsigned (1, 2 ou 3)
+  unsigned getPrioridade() const;
+    
+  /// @brief Busca pelo estado da tarefa
+  ///
+  /// @return String do estado (feito, em progresso, nao feito)
+  std::string getEstado() const; 
   
+  /// @brief Muda o titulo de uma tarefa
+  ///
+  /// @param novoTitulo O novo titulo a ser colocado
+  void setTitulo(const std::string& novoTitulo);
+
+  /// @brief Muda a descrição de uma tarefa
+  ///
+  /// @param novaDescricao A nova descrição a ser colocada
+  void setDescricao(const std::string& novaDescricao);
+
+  /// @brief Muda a data de uma tarefa
+  ///
+  /// @param novaData A nova data a ser colocada
+  ///
+  /// @attention A data informada deve estar no formato: DD/MM/AAAA
+  void setData(const std::string& novaData);
+
+  /// @brief Muda a prioridade de uma tarefa
+  ///
+  /// @param novaPrioridade A nova prioridade a ser colocada
+  ///
+  /// @attention A prioridade varia entre: 
+  /// 1 --> menos importante, 2 --> importante, 3 --> muito importante.
+  void setPrioridade(unsigned novaPrioridade);
+
+  /// @brief Muda o estado de uma tarefa
+  ///
+  /// @param novoEstado O novo estado a ser colocado
+  ///
+  /// @attention O estado varia entre: feito deletar em seguida), em progresso, não feito (alertar).
+  void setEstado(const std::string& novoEstado);
+   
+private:
+  std::string titulo; ///< Armazena o titulo da tarefa 
+  std::string descricao; ///< Armazena a descricao da tarefa
+  std::string data; ///< Armazena a data (em string) da tarefa 
+  unsigned prioridade; ///< Armazena a prioridade da tarefa 
+  std::string estado; ///< Armazena o estado da tarefa 
  };
 
 class Compromisso : public Tarefa {
-  public:
-    Compromisso(std::string& data, std::string& local,std::string& titulo,
-                std::string& descricao, std::string& horario, std::string& estado);
-    
-    std::string getLocal() const;
-    std::string getHorario() const;
+public:
+  Compromisso(std::string& data, std::string& local,std::string& titulo, std::string& descricao, std::string& horario, std::string& estado);
 
-    /// @brief Muda o local de um Compromisso
-    ///
-    /// @param NovoLocal       O novo local a ser colocado 
-    void setLocal(const std::string& novoLocal);
+  /// @brief Busca pelo local do compromisso
+  ///
+  /// @return String do local  
+  std::string getLocal() const;
 
-    /// @brief Muda o horário de um Compromisso
-    ///
-    /// @param NovoHorario       O novo horário a ser colocado
-    ///
-    /// @attention O Horario informado deve estar no formato:
-    /// HH:MM:SS
-    ///
-    void setHorario(const std::string& novoHorario);
+  /// @brief Busca pelo horario do compromisso
+  ///
+  /// @return String do horario 
+  std::string getHorario() const;
 
-  private:
-    std::string local;
-    std::string horario;
+  /// @brief Muda o local de um compromisso
+  ///
+  /// @param novoLocal O novo local a ser colocado 
+  void setLocal(const std::string& novoLocal);
+
+  /// @brief Muda o horário de um compromisso
+  ///
+  /// @param novoHorario O novo horario a ser colocado
+  ///
+  /// @attention O horario informado deve estar no formato: HH:MM:SS
+  void setHorario(const std::string& novoHorario);
+
+private:
+  std::string local; ///< Armazena o local do compromisso
+  std::string horario; ///< Armazena o horario do comprimisso
 };
-
