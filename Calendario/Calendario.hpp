042b7b2f089b5c8fa64aac4dd7e8521ebcf95f9a
rob1913r <148152111+rob1913r@users.noreply.github.com>
2023-11-22 14:36:37 -0300

    Update Calendario.hpp

--- Calendario/Calendario.hpp
+++ Calendario/Calendario.hpp
@@ -3,16 +3,16 @@
 #include <vector>
 #include <string>
 #include <map>
-#include "Tarefa/Tarefa.hpp"
-#include "Lembrete/Lembrete.hpp"
+#include "../Tarefa/Tarefa.hpp"
+#include "../Lembrete/Lembrete.hpp"
 
 class Calendario {
 public:
-    Calendario();  
+    //Calendario();  
 
     /// @brief Imprime o calendario da semana atual 
     void imprimirCalendario() const;
 
 private:
-    std::map<std::string, std::map<std::string, std::vector<std::string>>> agenda; ///< Mapa para armazenar eventos por dia e horario
+    std::map<std::string, std::map<std::string, std::vector<std::string>>> _agenda; ///< Mapa para armazenar eventos por dia e horario
 };
