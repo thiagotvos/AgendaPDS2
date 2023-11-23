2a4d9f401ca0f085ea35592acffa0e75ea7b9313
rob1913r <148152111+rob1913r@users.noreply.github.com>
2023-11-23 13:36:41 -0300

    Update Calendario.hpp

--- Calendario/Calendario.hpp
+++ Calendario/Calendario.hpp
@@ -8,11 +8,26 @@
 
 class Calendario {
 public:
-    //Calendario();  
+    /// @brief Insere um grupo de atividades em um determinado horário
+    ///
+    /// @param key O horario das atividades
+    ///
+    /// @param value As atividades que serão inseridas
+    void inserir(std::string key, std::vector<std::string> value);
 
-    /// @brief Imprime o calendario da semana atual 
-    void imprimirCalendario() const;
+    /// @brief Insere um horário com atividades(ou não) em um determinado dia
+    ///
+    /// @param key O dia
+    ///
+    /// @param value As atividades que serão inseridas
+    void inserir(std::string key);  
+
+    /// @brief Imprime o calendario do dia escolhido
+    ///
+    /// @param dia O dia escolhido
+    void imprimirCalendario(std::string dia);
 
 private:
-    std::map<std::string, std::map<std::string, std::vector<std::string>>> _agenda; ///< Mapa para armazenar eventos por dia e horario
+    std::map<std::string, std::vector<std::string>> _mapaHorario; ///< Mapa para armazenar as atividades por horário
+    std::map<std::string, std::map<std::string, std::vector<std::string>>> _agenda; ///< Mapa para armazenar os horários por dia
 };
